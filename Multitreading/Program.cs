24071ec699645a5c2f6b0eb9b968fe46a5ade0e6
paxawok <yurcherevach@gmail.com>
2025-02-25 19:13:29 +0200

    Initial commit

---
+++ Multitreading/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+class MyThread
+{
+    public int Count;
+    string thrdName;
+    public MyThread(string name)
+    {
+        Count = 0;
+        thrdName = name;
+    }
+    //Точка входу у потік
+    public void Run()
+    {
+        Console.WriteLine(thrdName + " starting.");
+        do
+        {
+            // Призупинення даного потоку
+            Thread.Sleep(500);
+            Console.WriteLine("In the thread " + thrdName + ", Count=" + Count);
+            Count++;
+        } while (Count < 10);
+        Console.WriteLine(thrdName + " is completed.");
+    }
+}
